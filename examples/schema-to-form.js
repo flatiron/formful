8c11ea037ad4644fee2f59091f98f88238e15ed7
Marak Squires <marak.squires@gmail.com>
2012-06-12 06:04:03 -0700

    [examples] Updating examples

---
+++ examples/schema-to-form.js
@@ -0,0 +1,33 @@
+var http        = require('http'),
+    formful     = require('../lib/formful');
+
+var resource = {
+  _resource: "Creature",
+  schema : {
+    "properties": {
+      "name": {
+          "description": "the name of your awesome new site",
+          "type": "string",
+          "default": "my awesome app",
+          "required": true
+      },
+      "port": {
+          "description": "the port your application should listen on",
+          "type": "number",
+          "default": 8080
+      },
+      "host": {
+          "description": "the host your application should listen on",
+          "type": "any",
+          "default": "localhost"
+      }
+    }
+  }
+};
+
+formful.create(resource, function (err, result) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(result);
+});
--- examples/server.js
+++ examples/server.js
@@ -19,8 +19,63 @@ var Creature = resourceful.define('creature', function () {
   //
   this.string('name');
   this.string('description');
+  
+
 
 });
+
+
+var resourceful = require('resourceful');
+
+var Creature = resourceful.define('creature', function () {
+  this.string('diet');
+//  this.bool('vertebrate');
+//  this.array('belly');
+
+  /*
+  this.timestamps();
+  
+  //
+  // Remark: All custom type validation is provided by `revalidator`
+  // see: https://github.com/flatiron/revalidator
+
+  //
+  // A required email address
+  //
+  this.string('email',    { format: 'email', default: 'marak.squires@gmail.com', required: true });
+  
+  //
+  // An optional homepage with a default value
+  //
+  this.string('homepage', { format: 'url', default: "http://monster.com" });
+
+  //
+  // An ip address
+  //
+  this.string('ip', { format: 'ip-address' });
+
+  //
+  // An date-time address
+  //
+  this.string('date-time', { format: 'date-time' });
+
+  //
+  // An ipv6 address
+  //
+  this.string('date', { format: 'date' });
+
+  //
+  // A color
+  //
+  this.string('color', { format: 'color' });
+
+  */
+
+  this.prototype.feed = function (food) {
+    this.belly.push(food);
+  };
+});
+
 
 var router = new director.http.Router();
 formful.extendRouter(router, Creature);
