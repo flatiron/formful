f9ffb94c233731cf62edf5ed5a53dda380cb2b4f
Marak Squires <marak.squires@gmail.com>
2012-06-13 01:26:40 -0700

    [example] Clean up server example

--- examples/server.js
+++ examples/server.js
@@ -4,77 +4,13 @@ var http        = require('http'),
     resourceful = require('resourceful'),
     restful     = require('restful');
 
-//
-// Create a new Creature resource using the Resourceful library
-//
-var Creature = resourceful.define('creature', function () {
-  //
-  // Specify a storage engine
-  //
-  this.use('memory');
 
-  //
-  // Specify some properties with validation
-  //
-  this.string('name');
-  this.string('description');
-
-});
-
-var resourceful = require('resourceful');
+var router = new director.http.Router();
 
 var Creature = resourceful.define('creature', function () {
   this.string('diet');
-//  this.bool('vertebrate');
-//  this.array('belly');
-
-  /*
-  this.timestamps();
-  
-  //
-  // Remark: All custom type validation is provided by `revalidator`
-  // see: https://github.com/flatiron/revalidator
-
-  //
-  // A required email address
-  //
-  this.string('email',    { format: 'email', default: 'marak.squires@gmail.com', required: true });
-  
-  //
-  // An optional homepage with a default value
-  //
-  this.string('homepage', { format: 'url', default: "http://monster.com" });
-
-  //
-  // An ip address
-  //
-  this.string('ip', { format: 'ip-address' });
-
-  //
-  // An date-time address
-  //
-  this.string('date-time', { format: 'date-time' });
-
-  //
-  // An ipv6 address
-  //
-  this.string('date', { format: 'date' });
-
-  //
-  // A color
-  //
-  this.string('color', { format: 'color' });
-
-  */
-
-  this.prototype.feed = function (food) {
-    this.belly.push(food);
-  };
 });
 
-
-
-var router = new director.http.Router();
 var formfulRouter = formful.createRouter(Creature);
 var restfulRouter = restful.createRouter(Creature);
 
