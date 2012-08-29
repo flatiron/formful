4b4cbaa207b2183fd86dd50f98fcfaf23665bbc1
Marak Squires <marak.squires@gmail.com>
2012-08-28 22:28:37 -0700

    [examples] Added Creature.feed and Creature.hit to example resource

--- examples/server.js
+++ examples/server.js
@@ -7,6 +7,35 @@ Creature.property('type', String, { default: "dragon", enum: ["Dragon", "Unicorn
 Creature.property('email', String, { format: "email" });
 Creature.property('life', Number, { default: 10, min: 0, max: 20 });
 
+Creature.feed = function (_id, options, callback) {
+  var self = this;
+  self.get(_id, function(err, creature){
+    if(err) {
+      return callback(err);
+    }
+    var life = creature.life + 1;
+    self.update(_id, { life: life }, function(err, result){
+      callback(null, creature.id + ' has been fed. life is: ' + result.life);
+    });
+  });
+}
+Creature.feed.remote = true;
+
+Creature.hit = function (_id, options, callback) {
+  var self = this;
+  self.get(_id, function(err, creature){
+    if(err) {
+      return callback(err);
+    }
+    var life = creature.life - 1;
+    self.update(_id, { life: life }, function(err, result){
+      callback(null, creature.id + ' has been hit. life is: ' + result.life);
+    });
+  });
+}
+Creature.hit.remote = true;
+
+
 var Database = resourceful.define('database', function(){
 
  this.string('type', {
